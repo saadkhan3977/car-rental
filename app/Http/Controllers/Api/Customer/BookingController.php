7bef5723149f4b4289c49db85ca4233b833fc846
saadkhan3977 <xaadkhan43@gmail.com>
2024-09-18 16:42:58 +0500

    car list

--- app/Http/Controllers/Api/Customer/BookingController.php
+++ app/Http/Controllers/Api/Customer/BookingController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\BaseController as BaseController;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Ride;
+use App\Models\Car;
 use Auth;
 
 class BookingController extends BaseController
@@ -19,7 +20,7 @@ class BookingController extends BaseController
     
     public function car_list()
     {
-        $data['rides'] = Car::with('rider')->where('user_id',Auth::user()->id)->where('status','complete')->get();
+        $data['rides'] = Car::where('status','active')->get();
         return $this->sendResponse($data, 'My Journey Lists');
     }
 }
