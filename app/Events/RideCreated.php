39403b5eba10859bb594de2c831c4926da22c849
saadkhan3977 <xaadkhan43@gmail.com>
2024-12-09 18:18:59 +0500

    message bug

--- app/Events/RideCreated.php
+++ app/Events/RideCreated.php
@@ -22,8 +22,8 @@ class RideCreated implements ShouldBroadcast
 
     public function broadcastOn()
     {
-        dd($this->message->rider_id);
-        return new Channel('rider-channel-2',$this->message->rider_id);  // Make sure it's public or private as per your use case
+        // dd($this->message->rider_id);
+        return new Channel('rider-channel-'.$this->message->rider_id);  // Make sure it's public or private as per your use case
     }
 
     public function broadcastWith()
