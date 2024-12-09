7679d1f84154ee1787c2a20fefebe8e52fbf2a40
saadkhan3977 <xaadkhan43@gmail.com>
2024-12-09 18:43:45 +0500

    message bug

--- app/Events/Customer.php
+++ app/Events/Customer.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class RideCreated implements ShouldBroadcast
+class Customer implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
