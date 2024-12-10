733bd2f62f62f7e05a11718afec440069209cdb6
saadkhan3977 <xaadkhan43@gmail.com>
2024-12-10 18:47:43 +0500

    message bug

--- app/Events/RideCreated.php
+++ app/Events/RideCreated.php
@@ -1,33 +1,29 @@
 <?php
 namespace App\Events;
 
-use App\Models\User;
 use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\BroadcastEvent;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class RideCreated
+class RideCreated implements ShouldBroadcast
 {
-    use SerializesModels;
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    // public $user;
     public $message;
 
     public function __construct($message)
     {
-        // $this->user = $user;
         $this->message = $message;
     }
 
     public function broadcastOn()
     {
-        // Check if the user is a rider or customer
-        if ($this->message->role == 'rider') {
-            return new Channel('ride-' . $this->message->rider_id);
-        }
-
-        return new Channel('customer-' . $this->message->rider_id);
+        // dd($this->message->rider_id);
+        return new Channel('rider-channel-'.$this->message->rider_id);  // Make sure it's public or private as per your use case
     }
 
     public function broadcastWith()
