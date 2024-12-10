bf1044e4aeb2e0c0b5072f051394a47392762a27
saadkhan3977 <xaadkhan43@gmail.com>
2024-12-10 17:56:29 +0500

    message bug

--- app/Events/RideCreated.php
+++ app/Events/RideCreated.php
@@ -3,11 +3,13 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Http\Request;
+use App\Models\Conversation;
+use App\Models\Message;
+use Auth;
 
 class RideCreated implements ShouldBroadcast
 {
@@ -23,7 +25,7 @@ class RideCreated implements ShouldBroadcast
     public function broadcastOn()
     {
         // dd($this->message->rider_id);
-        return new PrivateChannel('rider-channel-'.$this->message->rider_id);  // Make sure it's public or private as per your use case
+        return ['private-rider-channel-'.$this->message->rider_id];  // Make sure it's public or private as per your use case
     }
 
     public function broadcastWith()
