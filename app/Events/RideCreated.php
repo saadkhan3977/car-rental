f8530e149384f2aff07d0e18a75209a679f50915
saadkhan3977 <xaadkhan43@gmail.com>
2024-12-10 18:49:59 +0500

    message bug

--- app/Events/RideCreated.php
+++ app/Events/RideCreated.php
@@ -1,33 +1,43 @@
 <?php
 namespace App\Events;
 
+use App\Models\User;
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\BroadcastEvent;
 use Illuminate\Queue\SerializesModels;
 
-class RideCreated implements ShouldBroadcast
-{
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
 
+class RideCreated
+{
+    use SerializesModels;
+
+    // public $user;
     public $message;
 
     public function __construct($message)
     {
+        // $this->user = $user;
         $this->message = $message;
     }
 
     public function broadcastOn()
     {
-        // dd($this->message->rider_id);
-        return new Channel('rider-channel-'.$this->message->rider_id);  // Make sure it's public or private as per your use case
+        // Check if the user is a rider or customer
+        if ($this->message->role == 'rider') {
+            return new Channel('ride-' . $this->message->rider_id);
+        }
+
+        return new Channel('customer-' . $this->message->rider_id);
     }
 
     public function broadcastWith()
     {
         return ['message' => $this->message];
     }
+
 }
